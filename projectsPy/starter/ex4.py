d2da075ac9e6bc7e324cfc696ac8299dccef8dc5
brat <brat@fightklub.in>
2024-07-02 01:15:31 +0530

    Exercise files update starter

---
+++ projectsPy/starter/ex4.py
@@ -0,0 +1,17 @@
+cars = 100
+space_in_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_car
+average_passenger_per_car = passengers / cars_driven
+
+print ("there are", cars, "cars available")
+print("there are only", drivers, "drivers available")
+print("there will be", cars_not_driven, "empty cars today")
+print("we can transport", carpool_capacity, "people today")
+print("we have", passengers, "to carpool today.")
+print("we need to put about", average_passenger_per_car, "in each car")
+
+print("if we fill car with max capacity we would need", passengers / space_in_car ,"free cars")
